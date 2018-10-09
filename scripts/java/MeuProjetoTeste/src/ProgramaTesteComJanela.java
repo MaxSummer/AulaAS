ddaa29d8e26c115ce5fc1c7b4ca3e3c926187da2
MaxSummer <summerspx@gmail.com>
2018-10-08 22:42:31 -0300

    adicionei arquivos

---
+++ scripts/java/MeuProjetoTeste/src/ProgramaTesteComJanela.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Camargo
+ */
+import static javax.swing.JOptionPane.showMessageDialog;
+public class ProgramaTesteComJanela {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        showMessageDialog(null, "Luis");
+        
+        
+        
+    }
+    
+}
